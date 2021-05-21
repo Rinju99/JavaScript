567c277f4224390c9f3c5e3fbc46428454cc27a2
Rinju Sherpa <rinju54@gmail.com>
2021-05-20 23:49:52 -0500

    protoype inheritance in js

---
+++ JavaScript/Inheritance.js
@@ -0,0 +1,25 @@
+// Inheritance ...
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+Book.prototype.getSummary = function () {
+  return `${this.title} was written by ${this.author} in ${this.year}`;
+};
+
+function Magazine(title, author, year, month) {
+  Book.call(this, title, author, year);
+  this.month = month;
+}
+
+Magazine.prototype = Object.create(Book.prototype);
+
+// instantiate Magazine object
+const magz = new Magazine("People skills", "Dale Carnagie", "2020", "feb");
+
+//use Magazine constructor
+// Magazine.prototype.constructor = Magazine;
+
+console.log(magz.getSummary());
